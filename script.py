8ad7a87fd579a7dddde2240c423d3d3ee7f0972a
Antoine Rochebois <antoine.rochebois@insa-lyon.fr>
2021-02-12 16:59:30 +0000

    bgp mpls

--- script.py
+++ script.py
@@ -19,6 +19,7 @@
 
 import telnetlib
 import yaml
+import time
 
 
 def connect_to_host(global_conf):
@@ -38,10 +39,67 @@ def connect_to_host(global_conf):
 def configure_ospf(tn, conf):
     tn.write(b"conf t\n")
     num = conf["process-number"]
-    tn.write(b"no router ospf " + num.encode('ascii') + b"\n")
-    tn.write(b"router ospf " + num.encode('ascii') + b"\n")
+    if ("vrf" in conf.keys()):
+        vrf = conf["vrf"]
+        tn.write(b"no router ospf " + num.encode('ascii') + b" vrf "+ vrf.encode('ascii') +b"\n")
+        tn.write(b"router ospf " + num.encode('ascii') + b" vrf "+ vrf.encode('ascii') + b"\n")
+    else:
+        tn.write(b"no router ospf " + num.encode('ascii') + b"\n")
+        tn.write(b"router ospf " + num.encode('ascii') + b"\n")
+    if ("redistribute-bgp-subnets" in conf.keys()):
+        tn.write(b"redistribute bgp " + conf["redistribute-bgp-subnets"].encode('ascii') + b" subnets" + b"\n")
     for net in conf["networks"]:
         tn.write(b"network " + net["subnet"].encode('ascii') + b" " + net["mask"].encode('ascii') + b" area " + net["area"].encode('ascii') + b"\n")
+    tn.write(b"end\n")
+
+def configure_vrf(tn, conf):
+    for vrf in conf:
+        tn.write(b"conf t\n")
+        name = vrf["name"].encode('ascii')
+        tn.write(b"no ip vrf " + name + b"\n")
+        time.sleep(10)
+        tn.write(b"ip vrf " + name + b"\n")
+        if ("rd" in vrf.keys()):
+            as_number = vrf["as-number"].encode('ascii')
+            rd = vrf["rd"].encode('ascii')
+            tn.write(b"rd " + as_number + b":" + rd + b"\n")
+            tn.write(b"route-target both " + as_number + b":" + rd + b"\n")
+        tn.write(b"end\n")
+    
+
+def configure_bgp(tn, conf):
+    as_number = conf["as-number"].encode('ascii')
+    tn.write(b"conf t\n")
+    tn.write(b"no router bgp "+ as_number +b"\n")
+    tn.write(b"router bgp "+ as_number +b"\n")
+    if ("neighbors" in conf.keys()):
+        for nei in conf["neighbors"]:
+            nei_add = nei["address"].encode('ascii')
+            if ("remote-as" in nei.keys()):
+                tn.write(b"neighbor " + nei_add + b" remote-as " + nei["remote-as"].encode('ascii') + b"\n")
+            if ("update-source" in nei.keys()):
+                tn.write(b"neighbor " + nei_add + b" update-source " + nei["update-source"].encode('ascii') + b"\n")
+    if ("address-families" in conf.keys()):
+        for af in conf["address-families"]:
+            if ("vrf" in af.keys()):
+                vrf = af["vrf"].encode('ascii')
+                tn.write(b"address-family " + af["family"].encode('ascii') + b" vrf "+ vrf +b"\n")
+            else:
+                tn.write(b"address-family " + af["family"].encode('ascii') + b"\n")
+            if ("neighbors" in af.keys()):
+                for nei in af["neighbors"]:
+                    nei_add = nei["address"].encode('ascii')
+                    if ("activate" in nei.keys()):
+                        if (nei["activate"]):
+                            tn.write(b"neighbor " + nei_add + b" activate\n")
+                    if ("remote-as" in nei.keys()):
+                        tn.write(b"neighbor " + nei_add + b" remote-as "+ nei["remote-as"].encode('ascii') + b"\n")
+            tn.write(b"exit\n")
+    if ("networks" in conf.keys()):
+        for net in conf["networks"]:
+            subnet = net["address"].encode('ascii')
+            mask = net["mask"].encode('ascii')
+            tn.write(b"network " + subnet + b" mask "+ mask + b"\n")
     tn.write(b"end\n")
 
 
@@ -51,9 +109,15 @@ def configure_interface(tn, interface_conf):
     tn.write(b"int " + name.encode('ascii') + b"\n")
     address = interface_conf["address"]
     mask = ""
+    if ("vrf-forwarding" in interface_conf.keys()):
+        vrf = interface_conf["vrf-forwarding"].encode('ascii')
+        tn.write(b"ip vrf forwarding " + vrf + b"\n")
     if ("mask" in interface_conf.keys()):
         mask = interface_conf["mask"]
     tn.write(b"ip address " + address.encode('ascii') + b" " + mask.encode('ascii') + b"\n")
+    if ("mpls-ip" in interface_conf.keys()):
+        if (interface_conf["mpls-ip"]):
+            tn.write(b"mpls ip\n")
     if (interface_conf["no-shutdown"]):
         tn.write(b"no shutdown\n")
     tn.write(b"end\n")
@@ -103,6 +167,9 @@ if __name__ == "__main__":
             print("Configuration of", conf["global"]["hostname"])
             tn = connect_to_host(conf["global"])
             
+            if ("vrf" in conf.keys()):
+                configure_vrf(tn, conf["vrf"])
+
             if ("loopbacks" in conf.keys()):
                 for loopback_conf in conf["loopbacks"]:
                     configure_loopback(tn, loopback_conf)
@@ -115,7 +182,11 @@ if __name__ == "__main__":
                 configure_dhcp(tn, conf["dhcp"])
 
             if ("ospf" in conf.keys()):
-                configure_ospf(tn, conf["ospf"])
+                for ospf_conf in conf["ospf"]:
+                    configure_ospf(tn, ospf_conf)
+
+            if ("bgp" in conf.keys()):
+                configure_bgp(tn, conf["bgp"])
 
             tn.write(b"exit\n")
             print(tn.read_all().decode('ascii'))
