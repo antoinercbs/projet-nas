cef49eb76165448cbb47fd409c623ccb149c3f21
Antoine Rochebois <antoine.rochebois@insa-lyon.fr>
2021-02-13 11:52:06 +0000

    client autoconf

--- compile-config.py
+++ compile-config.py
@@ -76,6 +76,78 @@ def configure_internal_bgp(conf, bgp_conf):
                                     fam_conf["neighbors"].append(fam_nei_conf)
                 router["bgp"]["address-families"].append(fam_conf)
 
+def configure_vrf(conf, network_conf):
+    vpn_conf = network_conf["vpn"]
+    rd = 1
+    for vpn in vpn_conf:
+        for site in vpn["sites"]:
+            for router in conf["routers"]:
+                if router["global"]["hostname"] == site["pe-router"]["name"]:
+                    if not ("vrf" in router.keys()):
+                        router["vrf"] = []
+                    vrf_conf = dict()
+                    vrf_conf["name"] = vpn["vrf-name"]
+                    vrf_conf["rd"] = rd
+                    vrf_conf["as_number"] = network_conf["ibgp"]["as-number"]
+                    router["vrf"].append(vrf_conf)
+        rd += 1
+
+def configure_ce_bgp(conf, network_conf):
+    vpn_conf = network_conf["vpn"]
+    for vpn in vpn_conf:
+        for site in vpn["sites"]:
+            for router in conf["routers"]:
+                if router["global"]["hostname"] == site["ce-router"]["name"]:
+                    router["bgp"] = dict()
+                    router["bgp"]["as-number"] = site["ce-router"]["as-number"]
+                    router["bgp"]["networks"] = []
+                    router["bgp"]["neighbors"] = []
+                    for inter in site["ce-router"]["interfaces-to-local"]:
+                        for inter_conf in router["interfaces"]:
+                            if inter["name"] == inter_conf["name"]:
+                                net_conf = dict()
+                                net_conf["address"] = apply_mask(inter_conf["address"], inter_conf["mask"])
+                                net_conf["mask"] = inter_conf["mask"]
+                                router["bgp"]["networks"].append(net_conf)
+                    for pe_router in conf["routers"]:
+                        if pe_router["global"]["hostname"] == site["pe-router"]["name"]:
+                            nei_conf = dict()
+                            nei_conf["remote-as"] = network_conf["ibgp"]["as-number"]
+                            for pe_inter in pe_router["interfaces"]:
+                                if pe_inter["name"] == site["pe-router"]["interface-to-ce"]:
+                                    nei_conf["address"] = pe_inter["address"]
+                            router["bgp"]["neighbors"].append(nei_conf)
+
+def configure_ce_ospf(conf, network_conf):
+    vpn_conf = network_conf["vpn"]
+    for vpn in vpn_conf:
+        for site in vpn["sites"]:
+            for router in conf["routers"]:
+                if router["global"]["hostname"] == site["ce-router"]["name"]:
+                    router["ospf"] = []
+                    ospf_conf = dict()
+                    ospf_conf["process-number"] = "1"
+                    ospf_conf["networks"] = []
+                    for inter in site["ce-router"]["interfaces-to-local"]:
+                        for inter_conf in router["interfaces"]:
+                            if inter["name"] == inter_conf["name"]:
+                                net_conf = dict()
+                                net_conf["area"] = "0"
+                                net_conf["address"] = apply_mask(inter_conf["address"], inter_conf["mask"])
+                                net_conf["mask"] = invert_ip(inter_conf["mask"])
+                                ospf_conf["networks"].append(net_conf)
+                    for inter_conf in router["interfaces"]:
+                        if site["ce-router"]["interface-to-pe"] == inter_conf["name"]:
+                            net_conf = dict()
+                            net_conf["area"] = "0"
+                            net_conf["address"] = apply_mask(inter_conf["address"], inter_conf["mask"])
+                            net_conf["mask"] = invert_ip(inter_conf["mask"])
+                            ospf_conf["networks"].append(net_conf)
+                    router["ospf"].append(ospf_conf)
+
+
+
+
 
 
 
@@ -90,6 +162,9 @@ if __name__ == "__main__":
 
             configure_backbone_ospf(conf, network_conf["backbone-ospf"])
             configure_internal_bgp(conf, network_conf["ibgp"])
+            configure_vrf(conf, network_conf)
+            configure_ce_bgp(conf, network_conf)
+            configure_ce_ospf(conf, network_conf)
 
             with open('output.yaml', 'w') as output_file:
                 documents = yaml.dump(conf, output_file)
